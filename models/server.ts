9f8a37d4fbaa8c1b77387eaf9a8ca0995a83d73c
csparedesGesin <csparedes@gesin.com.ec>
2021-07-22 17:50:12 -0500

    Y el último ajuste

--- models/server.ts
+++ models/server.ts
@@ -97,7 +97,7 @@ class Server {
     admin.initializeApp({
       credential: admin.credential.cert({
         project_id: process.env["FIREBASE_PROJECT_ID"],
-        private_key: JSON.stringify(process.env["FIREBASE_PRIVATE_KEY"]),
+        private_key: process.env["FIREBASE_PRIVATE_KEY"].toString(),
         client_email: process.env["FIREBASE_CLIENT_EMAIL"],
       }),
     });
