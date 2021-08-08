84545e77fc7bc55a88540041ba74fdf5eee818e8
csparedesGesin <csparedes@gesin.com.ec>
2021-08-08 15:11:42 -0500

    Ajustes Ventas

--- database/connection.ts
+++ database/connection.ts
@@ -1,15 +1,15 @@
 import { Sequelize } from 'sequelize';
-const db = new Sequelize('bdd-backend','root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: false,
-    port: 3306
-})
-// const db = new Sequelize(process.env.CONN_BDD,process.env.CONN_USER , process.env.CONN_PASS, {
-//     host: process.env.HOST,
+// const db = new Sequelize('bdd-backend','root', '', {
+//     host: 'localhost',
 //     dialect: 'mysql',
 //     logging: false,
 //     port: 3306
 // })
+const db = new Sequelize(process.env.CONN_BDD,process.env.CONN_USER , process.env.CONN_PASS, {
+    host: process.env.HOST,
+    dialect: 'mysql',
+    logging: false,
+    port: 3306
+})
 
 export default db;
