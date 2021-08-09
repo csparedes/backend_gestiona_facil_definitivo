6d755a10d7b57036378101bed34d54f2929c1e8b
csparedesGesin <csparedes@gesin.com.ec>
2021-08-08 21:59:05 -0500

    Configuración BDD

--- database/connection.ts
+++ database/connection.ts
@@ -1,10 +1,5 @@
 import { Sequelize } from 'sequelize';
 
-// const db = new Sequelize('gesincom_gestionafacil', 'gesincom_csparedes' , 'csparedes:vgesincom', {
-//     host: 'gesin.com.ec',
-//     dialect: 'mysql',
-//     logging: false,
-// })
 const db = new Sequelize(process.env['CONN_BDD'], process.env['CONN_USER'] ,process.env['CONN_PASS'], {
     host: process.env['CONN_HOST'],
     dialect: 'mysql',
--- dist/database/connection.js
+++ dist/database/connection.js
@@ -1,11 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-// const db = new Sequelize('gesincom_gestionafacil', 'gesincom_csparedes' , 'csparedes:vgesincom', {
-//     host: 'gesin.com.ec',
-//     dialect: 'mysql',
-//     logging: false,
-// })
 const db = new sequelize_1.Sequelize(process.env['CONN_BDD'], process.env['CONN_USER'], process.env['CONN_PASS'], {
     host: process.env['CONN_HOST'],
     dialect: 'mysql',
