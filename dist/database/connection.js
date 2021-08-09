6ceb7953e5c974d7bcbf6967e2387bfa8f1bc93c
csparedesGesin <csparedes@gesin.com.ec>
2021-08-08 21:32:48 -0500

    Test1

--- dist/database/connection.js
+++ dist/database/connection.js
@@ -7,10 +7,15 @@ const sequelize_1 = require("sequelize");
 //     logging: false,
 //     port: 3306
 // })
-const db = new sequelize_1.Sequelize(process.env.CONN_BDD, process.env.CONN_USER, process.env.CONN_PASS, {
-    host: process.env.HOST,
+const db = new sequelize_1.Sequelize('gesincom_gestionafacil', 'gesincom_csparedes', 'csparedes:vgesincom', {
+    host: 'gesin.com.ec',
     dialect: 'mysql',
     logging: false,
 });
+// const db = new Sequelize(process.env.CONN_BDD, process.env.CONN_USER , process.env.CONN_PASS, {
+//     host: process.env.HOST,
+//     dialect: 'mysql',
+//     logging: false,
+// })
 exports.default = db;
 //# sourceMappingURL=connection.js.map
