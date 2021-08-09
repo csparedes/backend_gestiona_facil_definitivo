028240f651a4e79fc18a38d14fe720711ae91401
csparedesGesin <csparedes@gesin.com.ec>
2021-08-08 21:50:01 -0500

    Gestión de cambios

--- database/connection.ts
+++ database/connection.ts
@@ -1,19 +1,14 @@
 import { Sequelize } from 'sequelize';
-// const db = new Sequelize('bdd-backend', 'root' , '', {
-//     host: 'localhost',
+
+// const db = new Sequelize('gesincom_gestionafacil', 'gesincom_csparedes' , 'csparedes:vgesincom', {
+//     host: 'gesin.com.ec',
 //     dialect: 'mysql',
 //     logging: false,
-//     port: 3306
 // })
-const db = new Sequelize('gesincom_gestionafacil', 'gesincom_csparedes' , 'csparedes:vgesincom', {
-    host: 'gesin.com.ec',
+const db = new Sequelize(process.env.CONN_BDD, process.env.CONN_USER ,process.env.CONN_PASS, {
+    host: process.env.HOST,
     dialect: 'mysql',
     logging: false,
 })
-// const db = new Sequelize(process.env.CONN_BDD, process.env.CONN_USER , process.env.CONN_PASS, {
-//     host: process.env.HOST,
-//     dialect: 'mysql',
-//     logging: false,
-// })
 
 export default db;
