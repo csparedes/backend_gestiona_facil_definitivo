da7665a635563a4d86f2dc3e5cc9d27891e894c2
csparedesGesin <csparedes@gesin.com.ec>
2021-08-11 16:17:26 -0500

    Despliegue Final

--- database/connection.ts
+++ database/connection.ts
@@ -1,14 +1,14 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize('bdd-backend', 'root' ,'', {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: false,
-})
-// const db = new Sequelize(process.env['CONN_BDD'], process.env['CONN_USER'] ,process.env['CONN_PASS'], {
-//     host: process.env['CONN_HOST'],
+// const db = new Sequelize('bdd-backend', 'root' ,'', {
+//     host: 'localhost',
 //     dialect: 'mysql',
 //     logging: false,
 // })
+const db = new Sequelize(process.env['CONN_BDD'], process.env['CONN_USER'] ,process.env['CONN_PASS'], {
+    host: process.env['CONN_HOST'],
+    dialect: 'mysql',
+    logging: false,
+})
 
 export default db;
